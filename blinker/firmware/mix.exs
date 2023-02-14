a675f06a591e9b4c354c70e7bc88f27637c78435
Maxime Janvier <m.janvier@lancey.fr>
2023-02-14 22:42:58 +0100

    blinker

--- blinker/firmware/mix.exs
+++ blinker/firmware/mix.exs
@@ -29,28 +29,28 @@ defmodule HelloNerves.MixProject do
   defp deps do
     [
       # Dependencies for all targets
-      {:nerves, "~> 1.7.15", runtime: false},
-      {:shoehorn, "~> 0.8.0"},
-      {:ring_logger, "~> 0.8.3"},
-      {:toolshed, "~> 0.2.13"},
+      {:nerves, "~> 1.9", runtime: false},
+      {:shoehorn, "~> 0.9"},
+      {:ring_logger, "~> 0.9"},
+      {:toolshed, "~> 0.3"},
 
       # Dependencies for all targets except :host
-      {:nerves_runtime, "~> 0.11.6", targets: @all_targets},
-      {:nerves_pack, "~> 0.6.0", targets: @all_targets},
+      {:nerves_runtime, "~> 0.13", targets: @all_targets},
+      {:nerves_pack, "~> 0.7", targets: @all_targets},
 
       # Dependencies for specific targets
-      {:nerves_system_rpi, "~> 1.18", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.18", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.18", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.18", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.18", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.18", runtime: false, targets: :rpi4},
-      {:nerves_system_bbb, "~> 2.13", runtime: false, targets: :bbb},
-      {:nerves_system_osd32mp1, "~> 0.9", runtime: false, targets: :osd32mp1},
-      {:nerves_system_x86_64, "~> 1.18", runtime: false, targets: :x86_64},
+      {:nerves_system_rpi, "~> 1.22", runtime: false, targets: :rpi},
+      {:nerves_system_rpi0, "~> 1.22", runtime: false, targets: :rpi0},
+      {:nerves_system_rpi2, "~> 1.22", runtime: false, targets: :rpi2},
+      {:nerves_system_rpi3, "~> 1.22", runtime: false, targets: :rpi3},
+      {:nerves_system_rpi3a, "~> 1.22", runtime: false, targets: :rpi3a},
+      {:nerves_system_rpi4, "~> 1.22", runtime: false, targets: :rpi4},
+      {:nerves_system_bbb, "~> 2.17", runtime: false, targets: :bbb},
+      {:nerves_system_osd32mp1, "~> 0.13", runtime: false, targets: :osd32mp1},
+      {:nerves_system_x86_64, "~> 1.22", runtime: false, targets: :x86_64},
 
       {:nerves_leds, "~> 0.8.1"},
-      {:plug_cowboy, "~> 2.0"},
+      {:plug_cowboy, "~> 2.6"},
       {:ui, path: "../ui", targets: @all_targets, env: Mix.env()},
     ]
   end
--- blinker/ui/mix.exs
+++ blinker/ui/mix.exs
@@ -26,18 +26,18 @@ defmodule Ui.MixProject do
 
   defp deps do
     [
-      {:phoenix, "~> 1.6.9"},
-      {:phoenix_html, "~> 3.0"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.17.5"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.6"},
-      {:esbuild, "~> 0.4", runtime: Mix.env() == :dev && Mix.target() == :host},
+      {:phoenix, "~> 1.6"},
+      {:phoenix_html, "~> 3.3"},
+      {:phoenix_live_reload, "~> 1.4", only: :dev},
+      {:phoenix_live_view, "~> 0.18"},
+      {:floki, "~> 0.34", only: :test},
+      {:phoenix_live_dashboard, "~> 0.7"},
+      {:esbuild, "~> 0.6", runtime: Mix.env() == :dev && Mix.target() == :host},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.18"},
+      {:gettext, "~> 0.22"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
+      {:plug_cowboy, "~> 2.6"},
       {:tailwind, "~> 0.1", runtime: Mix.env() == :dev && Mix.target() == :host}
     ]
   end
